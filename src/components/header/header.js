bdf0454b0279d8dc4dfc87c78ac3a2b7c8da7621
ganesh.balasaheb <ganesh.balasaheb@maqta.ae>
2024-12-29 14:44:49 +0400

    Mobile css menu

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,18 +1,38 @@
-import { NavLink, Link } from "react-router";
 import "./header.css"
 import NavigationItem from "./navigationItem/navigationItem";
 import HomeIcon from '@mui/icons-material/Home';
 
 const Header = () => {
+  const openNav = () => {
+    document.getElementById("myNav").style.width = "70%";
+  }
+
+  const closeNav = () => {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  let menu = (
+    <ul>
+      <NavigationItem link="/" close={closeNav} exact>
+        <HomeIcon color="primary" />
+      </NavigationItem>
+      <NavigationItem link="/contact" close={closeNav}>Contact</NavigationItem>
+      <NavigationItem link="/aboutme" close={closeNav}>About me</NavigationItem>
+    </ul>
+  );
+
   return (
     <header className="Toolbar">
-      <ul className="NavigationItems">
-        <NavigationItem link="/" exact>
-          <HomeIcon color="primary" />
-        </NavigationItem>
-        <NavigationItem link="/contact">Contact</NavigationItem>
-        <NavigationItem link="/aboutme">About me</NavigationItem>
-      </ul>
+      <button className="Login">Login</button>
+      <span className="MobileMenu" onClick={openNav}>&#9776;</span>
+      <div id="myNav" className="overlay">
+        <a className="closebtn" onClick={closeNav}>&times;</a>
+        <div class="overlay-content">
+          {menu}
+        </div>
+      </div>
+      <div className="menu">
+        {menu}
+      </div>
     </header>
   );
 };
